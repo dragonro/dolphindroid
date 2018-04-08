a183f66d86221cdbb194f70a60d5c5fd5c2fd948
Gridranger <gridranger@gmail.com>
2018-04-08 22:45:08 +0200

    portrait directions fixed

--- app/src/main/java/com/gridranger/dolphindroid/ControllerActivity.java
+++ app/src/main/java/com/gridranger/dolphindroid/ControllerActivity.java
@@ -97,14 +97,14 @@ public class ControllerActivity extends Activity implements SensorEventListener 
         maskMap.put(R.id.button_plus, 1 << 4);
         maskMap.put(R.id.button_minus, 1 << 5);
         maskMap.put(R.id.button_home, 1 << 6);
-        maskMap.put(R.id.button_north, 1 << 7);
-        maskMap.put(R.id.button_south, 1 << 8);
-        maskMap.put(R.id.button_west, 1 << 9);
-        maskMap.put(R.id.button_east, 1 << 10);
-        maskMap.put(R.id.button_north_west, 1 << 7 | 1 << 9);
-        maskMap.put(R.id.button_north_east, 1 << 7 | 1 << 10);
-        maskMap.put(R.id.button_south_west, 1 << 8 | 1 << 9);
-        maskMap.put(R.id.button_south_east, 1 << 8 | 1 << 10);
+        maskMap.put(R.id.button_west, 1 << 7);
+        maskMap.put(R.id.button_east, 1 << 8);
+        maskMap.put(R.id.button_south, 1 << 9);
+        maskMap.put(R.id.button_north, 1 << 10);
+        maskMap.put(R.id.button_south_west, 1 << 7 | 1 << 9);
+        maskMap.put(R.id.button_north_west, 1 << 7 | 1 << 10);
+        maskMap.put(R.id.button_south_east, 1 << 8 | 1 << 9);
+        maskMap.put(R.id.button_north_east, 1 << 8 | 1 << 10);
 
         for (Map.Entry<Integer, Integer> entry : maskMap.entrySet()) {
             Button b = (Button) findViewById(entry.getKey());
